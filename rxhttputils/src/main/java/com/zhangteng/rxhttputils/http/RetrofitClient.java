2df0b281297f08c3b012772debb0332cd451092f
Swing <763263311@qq.com>
2020-06-26 00:00:45 +0800

    全局拦截器失效bug

--- rxhttputils/src/main/java/com/zhangteng/rxhttputils/http/RetrofitClient.java
+++ rxhttputils/src/main/java/com/zhangteng/rxhttputils/http/RetrofitClient.java
@@ -13,7 +13,6 @@ public class RetrofitClient {
     private okhttp3.OkHttpClient okHttpClient;
 
     private RetrofitClient() {
-        okHttpClient = OkHttpClient.getInstance().getClient();
         builder = new Retrofit.Builder()
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create());
@@ -35,6 +34,9 @@ public class RetrofitClient {
     }
 
     public Retrofit getRetrofit() {
+        if (okHttpClient == null) {
+            okHttpClient = OkHttpClient.getInstance().getClient();
+        }
         return builder.client(okHttpClient).build();
     }
 }
