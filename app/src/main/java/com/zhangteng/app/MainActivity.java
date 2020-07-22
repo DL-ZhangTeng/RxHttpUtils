4bfcb6a83948449cbce01b3df83165b3bff9861d
duoluo9 <763263368@qq.com>
2020-07-22 13:40:08 +0800

    迁移到androidx

--- app/src/main/java/com/zhangteng/app/MainActivity.java
+++ app/src/main/java/com/zhangteng/app/MainActivity.java
@@ -1,7 +1,7 @@
 package com.zhangteng.app;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.zhangteng.rxhttputils.R;
 
